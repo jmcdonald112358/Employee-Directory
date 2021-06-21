6ab903ad3c53c95a851d787019812338e86807fe
Justin McDonald <73570812+jmcdonald112358@users.noreply.github.com>
2021-06-21 02:18:27 -0600

    Added page functions

--- employee-directory/src/components/InitialPage.jsx
+++ employee-directory/src/components/InitialPage.jsx
@@ -5,15 +5,57 @@ import Header from './Header';
 import EmpCard from './EmpCard';
 
 class InitialPage extends Component {
-   //set state
+   //set initial state to store relevant info
+   state = {
+      search: "",
+      employees: [],
+      err: ""
+   };
 
    //run API call
+   componentDidMount() {
+      API.search()
+      .then(res => {
+         console.log(res);
+         this.setState({
+            employees: res.data.results.map((x, i) => ({
+               givenName: x.name.first,
+               familyName: x.name.last,
+               photo: x.picture.large,
+               email: x.email,
+               phone: x.phone,
+               city: x.location.city,
+               key: i
+            })),
+         });
+      })
+      .catch(err => console.log(err));
+   }
 
    //function to search employees for existing entries and update list
+   empSearch = (filter) => {
+      console.log('Search', filter);
+      const filtered = this.state.employees.filter((emp) => {
+         let values = Object.values(emp).join('').toLowerCase();
+         return values.indexOf(filter.toLowerCase()) !== -1;
+      });
+      this.setState({ employees: filtered });
+   };
 
-   //update state
+   //update state for input
+   input = (x) => {
+      this.setState({
+         [x.target.name]: x.target.value
+      });
+      console.log(this.state.search);
+   }
 
    //search function for submit button
+   btnSubmit = (x) => {
+      x.preventDefault();
+      console.log(this.state.search, x);
+      this.empSearch(this.state.search);
+   };
 
    render() {
       return (
